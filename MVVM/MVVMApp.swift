35a66c4663576db8ef3fe36244e57dfbb0128274
Arsentiy Gonda <arsentiy.gonda@gmail.com>
2024-03-21 12:31:33 +0700

    Initial empty project

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by HITSStudent on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
